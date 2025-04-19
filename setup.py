1afcb85b8ac6e452f68c74a91c870f5362012a78
mario <mariomamdouh509@gmail.com>
2025-04-19 06:43:34 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages,setup
+
+
+
+
+
+
+setup(
+    name = 'END2ENDP'
+    version = '0.1',
+    author = 'mariomamdouh', 
+    author_email = "marioammdouh509@gmail.com" 
+    pacakages = find_packages(),
+    description = 'End to End Project', 
+    install_requires = 
+    [
+        'pandas', 'NUMPY', 'matplotlib', 'seaborn', 'scikit-learn', 'tensorflow', 'keras'
+    ]
+    
+)
